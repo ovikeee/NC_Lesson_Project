c283af93e486a6d6c040cbc235ecd4ed303a89cc
ovikeee <OvIk_94_@mail.ru>
2017-10-12 00:56:21 +0400

    Created project.

---
+++ src/main/java/com/netcracker/ssau/restplacesearcher/RestPlaceSearcherApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker.ssau.restplacesearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaceSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPlaceSearcherApplication.class, args);
+	}
+}
