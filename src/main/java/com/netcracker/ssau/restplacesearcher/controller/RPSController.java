2183b1bcf5a3a3c326d335d8acf4b8ddc6e9c7a5
Oganes <OvIk_94_@mail.ru>
2017-11-28 20:03:07 +0400

    Update RPSController.java
    Тест google web service

--- src/main/java/com/netcracker/ssau/restplacesearcher/controller/RPSController.java
+++ src/main/java/com/netcracker/ssau/restplacesearcher/controller/RPSController.java
@@ -1,10 +1,31 @@
 package com.netcracker.ssau.restplacesearcher.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 public class RPSController {
-/*    @RequestMapping(value = "/place", method = RequestMethod.GET)
-    public List<Place> getAllPlace() {
-    }*/
+
+    @RequestMapping(value = "/place", method = RequestMethod.GET)
+    public List getAllPlace() throws InterruptedException, ApiException, IOException {
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("AIzaSyB2ifm7v5evu58yCybWWlsKAVl9EoxTlaw")
+                .build();
+        GeocodingResult[] results = GeocodingApi.geocode(context,
+                "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(results[0].addressComponents));
+
+        return null;
+    }
 }
