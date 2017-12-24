804b1b1b9d3b02a14096b105386d510c6e19f84e
aerialist7 <32072831+aerialist7@users.noreply.github.com>
2017-12-24 19:33:05 +0400

    Сделал связующую логику, на уровне JS. Для проверки методов используются тестовые алерты. Добавил проверки на корректность вводимых атрибутов

---
+++ src/main/resources/static/restplacesearcher.js
@@ -0,0 +1,82 @@
+function navigateToStartPlace() {
+    const place = document.getElementById("start-place-input").value;
+    if (place) {
+        alert("Navigate to " + place);
+    }
+}
+
+function navigateToFinalPlace() {
+    const place = document.getElementById("final-place-input").value;
+    if (place) {
+        alert("Navigate to " + place);
+    }
+}
+
+function iAmLucky() {
+    alert("I'm lucky");
+}
+
+function findWay() {
+    alert("Way was found!");
+}
+
+function getWaterPlaces() {
+    alert("Water Places!");
+}
+
+function getArchitecturePlaces() {
+    alert("Architecture Places!");
+}
+
+function getBestPlaces() {
+    alert("Best Places!");
+}
+
+function findByType() {
+    const type = document.getElementById("place-type-input").value;
+    if (type) {
+        alert("Place with type " + type + " found!");
+    }
+}
+
+function filterByTemperature() {
+    const from = document.getElementById("from-select").value;
+    const to = document.getElementById("to-select").value;
+    if (from !== "null" && to !== "null" && from < to) {
+        alert("Places by temperature from " + from + " to " + to + " found!");
+    } else {
+        alert("Введите корректный диапазон или отключите температурный фильтр")
+    }
+}
+
+function filterByDistance() {
+    const distance = document.getElementById("distance-input").value;
+
+    if (distance) {
+        alert("Places by distance " + distance + " found!");
+    }
+}
+
+function filterByTime() {
+    const time = document.getElementById("time-input").value;
+
+    if (time) {
+        alert("Places by time " + time + " found!");
+    }
+}
+
+function filterPlaces() {
+    const temperatureFilter = document.getElementById("weather-checkbox").checked;
+    const distanceFilter = document.getElementById("distance-checkbox").checked;
+    const timeFilter = document.getElementById("time-checkbox").checked;
+
+    if (temperatureFilter === true) {
+        filterByTemperature();
+    }
+    if (distanceFilter === true) {
+        filterByDistance();
+    }
+    if (timeFilter === true) {
+        filterByTime();
+    }
+}
